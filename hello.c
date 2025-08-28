a98ae49273b67513e1a95c1ddc44382c090da462
sdelhay2 <sdelhay2@huskers.unl.edu>
2025-08-28 09:59:28 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Solomon Delhay
+ * Date: 2025/08/28
+ *
+ * A simple program that prints my name in C
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Solomon Delhay\n");
+
+    return 0;
+}
